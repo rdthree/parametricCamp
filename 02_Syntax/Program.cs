914d49debb6da3b92d595a8e32781a71089aa010
originalsurfmex <1165540+originalsurfmex@users.noreply.github.com>
2020-12-01 18:51:06 -0500

    Add project files.

---
+++ 02_Syntax/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // single line comment
+            // another single line comment
+
+            //future comment - chord ctrl+k ctrl+/
+
+            /*
+             * multi
+             * line
+             * comment
+             */
+
+            //print a message to the console
+            Console.WriteLine("Sup Dudes");
+            Console.ReadKey(); //stop program, wait for user keyboard input
+
+        }
+    }
+}
